36529a789d28e85c74e7b80652807cb2d010c50b
Dávid Hezký <hezky@epos.cd.cz>
2019-03-13 13:06:07 +0100

    fix: build vytvareni adresare lib

--- src/js/utils/build.js
+++ src/js/utils/build.js
@@ -4,7 +4,7 @@ const spawnSync = require("child_process").spawnSync;
 
 /* eslint-disable no-console */
 const compileBabel = (source, target) => {
-  if (existsSync("lib")) mkdirSync("lib");
+  if (!existsSync("lib")) mkdirSync("lib");
   const args = [source, "--comments=false", "--out-dir", target];
   const res = spawnSync("babel", args);
   if (res.status !== 0) {
