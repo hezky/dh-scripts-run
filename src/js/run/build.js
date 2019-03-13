0f30ff9e625180f8e354de4e4f31982b7241f7a5
Dávid Hezký <hezky@epos.cd.cz>
2019-03-13 13:00:14 +0100

    fix: config babelrc

--- src/js/run/build.js
+++ src/js/run/build.js
@@ -1,5 +1,5 @@
-const DIR_LIB = require("./../consts/dirs.js").lib;
-const DIR_SRC = require("./../consts/dirs.js").src;
+const DIR_LIB = require("./../consts/dirs.js").LIB;
+const DIR_SRC = require("./../consts/dirs.js").SRC;
 const compileBabel = require("./../utils/build.js").compileBabel;
 
 compileBabel(DIR_SRC, DIR_LIB);
--- src/js/utils/build.js
+++ src/js/utils/build.js
@@ -3,7 +3,7 @@ const spawnSync = require("child_process").spawnSync;
 
 /* eslint-disable no-console */
 const compileBabel = (source, target) => {
-  mkdirSync(target);
+  mkdirSync("lib");
   const args = [source, "--comments=false", "--out-dir", target];
   const res = spawnSync("babel", args);
   if (res.status !== 0) {
