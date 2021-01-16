6cdde9dfa180f665532cc1242fb71e089ecbf1d2
Dávid Hezký <david.hezky@cdis.cz>
2021-01-16 23:06:34 +0100

    fix: prepublish changed to prepublishOnly

--- src/js/run/prepublishOnly.js
+++ src/js/run/prepublishOnly.js
@@ -1,6 +1,6 @@
-import { run } from "scripts/prepublish";
+import { run } from "scripts/prepublishOnly";
 
 const lastScriptsRun = process.env.DH_SCRIPTS_RUN;
-process.env.DH_SCRIPTS_RUN = "PREPUBLISH";
+process.env.DH_SCRIPTS_RUN = "PREPUBLISHONLY";
 run();
 process.env.DH_SCRIPTS_RUN = lastScriptsRun;
