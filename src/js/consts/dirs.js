8c22c2e88c60912a048151a8a2697ebfe91bc90b
Dávid Hezký <david.hezky@cdis.cz>
2021-02-15 21:27:24 +0100

    rm: const/dirs node_modules

--- src/js/consts/dirs.js
+++ src/js/consts/dirs.js
@@ -1,15 +1,15 @@
 const DIR_DIST = "dist";
 const DIR_LIB = "lib";
-const DIR_LIB_JS = "lib/js/";
-const DIR_NODE_MODULES = "node_modules/";
-const DIR_SRC_JS = "src/js/";
-const DIR_TEST = "test/";
+const DIR_LIB_JS = `${DIR_LIB}/js`;
+
+const DIR_SRC_JS = "src/js";
+const DIR_TEST = "test";
 
 const CWD = process.cwd();
 const CWD_DIST = `${CWD}/${DIR_DIST}`;
 const CWD_LIB = `${CWD}/${DIR_LIB}`;
 const CWD_LIB_JS = `${CWD}/${DIR_LIB_JS}`;
-const CWD_NODE_MODULES = `${CWD}/${DIR_NODE_MODULES}/`;
+
 const CWD_SRC_JS = `${CWD}/${DIR_SRC_JS}`;
 const CWD_TEST = `${CWD}/${DIR_TEST}`;
 
@@ -18,7 +18,6 @@ export {
   CWD_DIST,
   CWD_LIB,
   CWD_LIB_JS,
-  CWD_NODE_MODULES,
   CWD_SRC_JS,
   CWD_TEST,
   DIR_LIB,
