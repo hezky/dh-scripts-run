d064fb74f285a6e766d3678b520b9b8a10f086cd
Dávid Hezký <david.hezky@cdis.cz>
2021-04-05 20:57:34 +0200

    fix: folder consts

--- src/js/run/consts/scriptNames.js
+++
@@ -1,7 +0,0 @@
-const BUILD = "build";
-const CLEAN = "clean";
-const ESLINT = "eslint";
-const PREPUBLISH_ONLY = "prepublishOnly";
-const TEST = "test";
-
-export { BUILD, CLEAN, ESLINT, PREPUBLISH_ONLY, TEST };
