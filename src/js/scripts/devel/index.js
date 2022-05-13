f18ca242694ec9a79493ed697eecd50ff76ebd90
Dávid Hezký <david.hezky@cdis.cz>
2022-05-13 11:14:58 +0200

    mod: update script devel for global run

--- src/js/scripts/devel/index.js
+++ src/js/scripts/devel/index.js
@@ -1,12 +1,14 @@
 import { existsSync } from "fs";
 const spawnSync = require("child_process").spawnSync;
 
-import { CWD_DEVEL } from "consts/dirs";
+import { CWD_DEVEL, RUN_MODULES } from "consts/dirs";
+
+const RUN_BABEL = `${RUN_MODULES}/@babel/node/bin/babel-node.js`;
 
 const run = () => {
   const path = `${CWD_DEVEL}/index.js`;
   if (existsSync(path)) {
-    spawnSync("babel-node", [require.resolve(path)], {
+    spawnSync(RUN_BABEL, [require.resolve(path)], {
       stdio: "inherit",
     });
   }
