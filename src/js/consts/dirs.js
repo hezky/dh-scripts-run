398952dd4d8ff46fdf8e7097a5023885f397a19d
Dávid Hezký <david.hezky@cdis.cz>
2022-11-06 17:49:07 +0100

    fix: path to node_modules

--- src/js/consts/dirs.js
+++ src/js/consts/dirs.js
@@ -24,10 +24,8 @@ const CWD_SRC_JS = `${CWD}/${DIR_SRC_JS}`;
 const CWD_TEST = `${CWD}/${DIR_TEST}`;
 
 const DEEP_PARRENT_FOLDER =
-  Boolean(process.env.isNpmRun) !== true ||
-  Boolean(process.env.isScriptsRun) === true
-    ? 3
-    : 6;
+  process.env.isNpmRun == "false" || process.env.isScriptsRun == "true" ? 3 : 6;
+
 const RUN = PARRENT_FOLDER(__dirname, DEEP_PARRENT_FOLDER);
 const RUN_MODULES = `${RUN}/${DIR_NODE_MODULES}`;
 
