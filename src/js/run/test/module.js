840faf8844a5a5a43cd585844764e0175e0905d7
Dávid Hezký <david.hezky@cdis.cz>
2022-11-06 16:45:17 +0100

    fix: test access denied

--- src/js/run/test/module.js
+++ src/js/run/test/module.js
@@ -11,6 +11,7 @@ const RUN_MOCHA = `${RUN_MODULES}/mocha/bin/mocha.js`;
 const testMocha = () => {
   try {
     fs.accessSync(CWD_TEST_MOCHA);
+    fs.chmodSync(RUN_MOCHA, 0o777);
     const args = ["--require", RUN_BABEL, "--recursive", CWD_TEST_MOCHA];
     const res = spawnSync(RUN_MOCHA, args, { stdio: "inherit" });
     if (res.status !== 0) {
