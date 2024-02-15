8d1aefc5811f3b87451e3910e0e03fb52ceeacfe
David Hezky <david.hezky@seznam.cz>
2024-02-15 21:58:57 +0100

    fix: build

--- src/js/run/commands/build/module.js
+++ src/js/run/commands/build/module.js
@@ -1,27 +1,16 @@
 import { existsSync, mkdirSync } from "fs";
 import { spawnSync } from "child_process";
 
-import { CWD_LIB_JS, CWD_SRC_JS, RUN_MODULES } from "consts/dirs";
+import { CWD_LIB_JS, CWD_SRC_JS } from "consts/dirs";
+import { dh__get__controlledPath } from "consts/env";
 import { logError } from "utils/log";
 
 // const RUN_BABEL = `${RUN_MODULES}/@babel/cli/bin/babel.js`;
-const RUN_BABEL = `/home/jsDH/dh-scripts-run/node_modules/@babel/cli/bin/babel.js`;
 
 const compileBabel = (source, target) => {
-  if (!existsSync(CWD_SRC_JS)) {
-    console.log("--- neexistuje src");
-  } else {
-    console.log("--- existuje src");
-  }
-  if (!existsSync(CWD_LIB_JS)) {
-    console.log("--- neexistuje lib");
-  } else {
-    console.log("--- existuje lib");
-  }
+  const RUN_BABEL = `${dh__get__controlledPath()}/node_modules/@babel/cli/bin/babel.js`;
   if (!existsSync(CWD_LIB_JS)) mkdirSync(CWD_LIB_JS, { recursive: true });
   const args = [source, "--no-comments", "--out-dir", target];
-  console.log("Run: ", RUN_BABEL);
-  console.log(`Compiling Babel: ${args.join(" ")}`);
   const res = spawnSync(RUN_BABEL, args);
   if (res.status !== 0) {
     logError(res);
