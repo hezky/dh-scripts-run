41db479342ff5932a833648c1fab5dc1ae51dbd0
David Hezky <david.hezky@seznam.cz>
2024-02-17 23:17:17 +0100

    mod: build - replace babel-cli to babel-core

--- src/js/run/commands/build/module.js
+++ src/js/run/commands/build/module.js
@@ -1,3 +1,54 @@
+import {
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  readFileSync,
+  writeFileSync,
+  statSync,
+} from "fs";
+import { join } from "path";
+import { transformSync } from "@babel/core";
+
+import { CWD_LIB_JS, CWD_SRC_JS } from "consts/dirs";
+import { logError } from "utils/log";
+
+const compileBabel = (source, target) => {
+  if (!existsSync(target)) mkdirSync(target, { recursive: true });
+
+  const files = readdirSync(source);
+
+  for (const file of files) {
+    const sourcePath = join(source, file);
+    const targetPath = join(target, file);
+
+    if (statSync(sourcePath).isDirectory()) {
+      compileBabel(sourcePath, targetPath);
+    } else {
+      try {
+        const code = readFileSync(sourcePath, "utf-8");
+        const result = transformSync(code, {
+          presets: ["@babel/preset-env"],
+          comments: false,
+        });
+
+        if (result && result.code) {
+          writeFileSync(targetPath, result.code);
+        }
+      } catch (error) {
+        logError(error);
+      }
+    }
+  }
+};
+
+const module = () => {
+  compileBabel(CWD_SRC_JS, CWD_LIB_JS);
+};
+
+export default module;
+
+/*
+// backup original file
 import { existsSync, mkdirSync } from "fs";
 import { spawnSync } from "child_process";
 
@@ -19,3 +70,4 @@ const module = () => {
 };
 
 export default module;
+*/
